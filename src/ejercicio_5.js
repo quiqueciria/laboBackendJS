3ad46df0edf2d32ea4020fb61973716851f4a859
Quique Ciria <quiqueciria@gmail.com>
2023-09-05 16:55:36 +0200

    exercise up to the fith

--- src/ejercicio_5.js
+++ src/ejercicio_5.js
@@ -2,18 +2,19 @@
 
 const elements = [0, 1, false, 2, "", 3];
 
-const compact = (arg) => {};
+const compact = elements.filter((element) => element);
 
-console.log(compact(123)); // 123
-console.log(compact(null)); // null
-console.log(compact([0, 1, false, 2, "", 3])); // [1, 2, 3]
-console.log(compact({})); // {}
-console.log(
-  compact({
-    price: 0,
-    name: "cloud",
-    altitude: NaN,
-    taste: undefined,
-    isAlive: false,
-  })
-); // {name: "cloud"}
+console.log(compact); // 123
+
+// console.log(compact(null)); // null
+// console.log(compact([0, 1, false, 2, "", 3])); // [1, 2, 3]
+// console.log(compact({})); // {}
+// console.log(
+//   compact({
+//     price: 0,
+//     name: "cloud",
+//     altitude: NaN,
+//     taste: undefined,
+//     isAlive: false,
+//   })
+// ); // {name: "cloud"}
