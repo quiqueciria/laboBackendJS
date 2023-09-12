de41afaa817d831654b0da7c6e7631512b5d2e1c
Quique Ciria <quiqueciria@gmail.com>
2023-09-12 11:08:48 +0200

    status

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 // EJERCICIO 5
 
-const elements = [0, 1, false, 2, "", 3];
+// ----OK------- eliminar valores falsys de una estructura de datos. Utilizamos el método
+// filter para que nos devuelva los elementos true.
 
-const compact = (arg) => {
-  if (Array.isArray(arg)) {
-    return arg.filter(Boolean);
-  } else {
-    return arg;
-  }
-};
+// const elements = [0, 1, false, 2, "", 3];
+// const compact = (arg) => {
+//   const truthyElements = elements.filter((element) => element);
+//   return truthyElements;
+// };
 
-console.log(compact(123)); // 123
+// console.log(compact(123)); // 123
 
-//console.log(compact(null)); // null
-// console.log(compact([0, 1, false, 2, "", 3])); // [1, 2, 3]
+// ----PENDING------- Si el argumento es un objeto, deberá eliminar sus propiedades falsys.
+// No entiendo lo del si el argumento es un objeto
+
+// const elements = [0, 1, false, 2, "", 3];
+// const compact = (arg) => {};
+
+// console.log(compact(null)); // null
+
+// ----OK----- Si el argumento es un array, deberá eliminar los elementos falsys.
+// const elements = [0, 1, false, 2, "", 3];
+// const compact = (arg) => {
+//   const truthyElements = elements.filter((element) => element);
+//   return truthyElements;
+// };
+
+// console.log(compact(123)); // 123
+
+// ----PENDING-------  Si el argumento es un objeto o un array no deberán ser mutados.
+// No entiendo. Cuando comparo, elements y compact me dicen que no son iguales,
+// por tanto, no son mutados, no?
+
+// const elements = [0, 1, false, 2, "", 3];
+
+// const compact = (arg) => {
+//   const truthyElements = elements.filter((element) => element);
+//   return truthyElements;
+// };
+
 // console.log(compact({})); // {}
+// console.log(elements === compact);
+
+// ----------- Si no es ni un objeto ni un array deberá de devolver dicho argumento.
 // console.log(
 //   compact({
 //     price: 0,
